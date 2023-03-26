bf703ef7852ca19dd45f07535e0c904aaef91a58
lujoh <lucyjohnson1995@gmail.com>
2023-03-26 12:16:20 -0400

    added environment variables and changed basemap to API Key required basemap

--- src/data/MapStructure.jsx
+++ src/data/MapStructure.jsx
@@ -1,9 +1,10 @@
 import Map from "@arcgis/core/Map"
 import MapView from "@arcgis/core/views/MapView";
+import esriConfig from "@arcgis/core/config";
 
-
+esriConfig.apiKey = import.meta.env.VITE_ARCGIS_API_KEY;
 export const map = new Map({
-    basemap: "satellite"
+    basemap: "arcgis-dark-gray"
 });
 
 export const view = new MapView({
