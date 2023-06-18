81a4c78f208fa2a65005cd7bc07eeb5390fcb5e0
lujoh <lucyjohnson1995@gmail.com>
2023-06-18 18:08:54 -0400

    rearraged application layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,8 @@ function App() {
 
   return (
     <div className="app">
-      <header className='app_header'><h1>Owls of Bavaria</h1></header>
-      <div className='app_row'>
-        <MapWindow />
-        <Sidebar />
-      </div>
+      <Sidebar />
+      <MapWindow />
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,7 @@ function Sidebar() {
 
   return (
       <div className="sidebar">
+        <header className='sidebar_header'><h1>Owls of Bavaria</h1></header>
         <SpeciesSection />
         <div>
           <p>Owl observations courtesy of iNaturalist and the iNaturalist contributors. To add your own observations or suggest a different species id, go to the <a href='https://www.inaturalist.org/' target='blank'>iNaturalist website.</a></p>
